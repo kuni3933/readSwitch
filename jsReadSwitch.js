410e8abe23325604e5633122e94abd75b9f0dca8
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-11-30 16:14:07 +0900

    [Update]feat: sleepを導入

--- jsReadSwitch.js
+++ jsReadSwitch.js
@@ -1,12 +1,20 @@
 import { Gpio } from "pigpio";
+import { sleep } from "sleep";
 
-const PIN = 18;
+const PIN = 11;
 // GPIO(11pin)を入力モードとし、pull up設定とする
 const readSwitch = new Gpio(PIN, {
   mode: Gpio.INPUT,
   pullUpDown: Gpio.PUD_UP,
 });
 
+let isOpened = false;
 while (true) {
-  console, log(`readSwitch: ${readSwitch.digitalRead()}`);
+  const whileIsOpened = readSwitch.digitalRead();
+  if (isOpened != whileIsOpened) {
+    isOpened = whileIsOpened;
+    console.log(`isOpened: ${whileIsOpened}`);
+  }
+  sleep(10);
+  console.log("10 sec out");
 }
